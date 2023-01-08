15b04bf312a3f978836122ffdf439a857fc7d0c3
IliaNoah <vaguaman86@gmail.com>
2023-01-08 12:48:22 +0330

    finished js part on ham button

---
+++ script.js
@@ -0,0 +1,12 @@
+const navButton = document.querySelector('.nav-button');
+const navMenu = document.querySelector('.nav-menu');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+
+navButton.addEventListener('click', function() {
+    line1.classList.toggle('change');
+    line2.classList.toggle('change');
+    line3.classList.toggle('change');
+    navMenu.classList.toggle('bg-dark');
+});
