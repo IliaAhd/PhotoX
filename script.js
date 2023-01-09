5e4a037491b8aa6b9be3a7a864750e7a780160f2
IliaNoah <vaguaman86@gmail.com>
2023-01-09 17:23:27 +0330

    finished navbar

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const navButton = document.querySelector('.nav-button');
-const navMenu = document.querySelector('.nav-menu');
-const line1 = document.querySelector('.line1');
-const line2 = document.querySelector('.line2');
-const line3 = document.querySelector('.line3');
+const navButton = document.querySelector(".nav-button");
+const navMenu = document.querySelector(".nav-menu");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
 
-navButton.addEventListener('click', function() {
-    line1.classList.toggle('change');
-    line2.classList.toggle('change');
-    line3.classList.toggle('change');
-
-    if(navMenu.classList.contains('.bg-dark')) {
-        navMenu.classList.remove('bg-dark');
-    } else {
-        navMenu.classList.add('bg-dark');
-    }
+navButton.addEventListener("click", function () {
+  line1.classList.toggle("change");
+  line2.classList.toggle("change");
+  line3.classList.toggle("change");
+  navMenu.classList.toggle("bg-dark");
 });
+
+window.onscroll = function() {
+  if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
+    navMenu.classList.add('costum-navbar');
+} else {
+    navMenu.classList.remove('costum-navbar');
+}
+};
