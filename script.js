85e300ae5ad2989bc9ac36ed7b8439e3b33324da
IliaNoah <vaguaman86@gmail.com>
2023-01-10 16:39:09 +0330

    animated mission second part

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 
+const cameraImage = document.querySelector(".camera-img");
+const missionText = document.querySelector(".mission-text");
+
 navButton.addEventListener("click", function () {
   line1.classList.toggle("change");
   line2.classList.toggle("change");
@@ -11,10 +14,24 @@ navButton.addEventListener("click", function () {
   navMenu.classList.toggle("bg-dark");
 });
 
-window.onscroll = function() {
-  if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
-    navMenu.classList.add('costum-navbar');
-} else {
-    navMenu.classList.remove('costum-navbar');
-}
+window.onscroll = function () {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    navMenu.classList.add("costume-navbar");
+  } else {
+    navMenu.classList.remove("costume-navbar");
+  }
+
+  if (
+    document.body.scrollTop >= 650 ||
+    document.documentElement.scrollTop >= 650
+  ) {
+    missionText.classList.add("from-right");
+    cameraImage.classList.add("from-left");
+  } else {
+    missionText.classList.remove("from-right");
+    cameraImage.classList.remove("from-left");
+  }
 };
