15247e4880f08ad083f591dfa94f29a340d0b4b7
IliaNoah <vaguaman86@gmail.com>
2023-05-05 18:16:03 +0430

    refactor item appears using intersection obs api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // Items & elements
 const navButton = document.querySelector(".nav-button");
@@ -10,9 +10,11 @@ const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 
+const missionSection = document.querySelector(".mission-appear");
 const cameraImage = document.querySelector(".camera-img");
 const missionText = document.querySelector(".mission-text");
 
+const pricingSection = document.querySelector("#pricing");
 const card1 = document.querySelector(".card-1");
 const card2 = document.querySelector(".card-2");
 const card3 = document.querySelector(".card-3");
@@ -49,50 +51,60 @@ for (let i = 0; i < galleryItems.length; i++) {
   });
 }
 
+const observerOption = {
+  root: null,
+  treshold: 0.1,
+};
+
 // Sticky Navbar
 const navChange = function (entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) navMenu.classList.add("costume-navbar");
   else navMenu.classList.remove("costume-navbar");
 };
 
-const navObserver = new IntersectionObserver(navChange, {
-  root: null,
-  treshold: 0.1,
-});
+const navObserver = new IntersectionObserver(navChange, observerOption);
 navObserver.observe(banner);
 
+// Mission section pictures appear
+const missionAppear = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    missionText.classList.add("from-right");
+    cameraImage.classList.add("from-left");
+    document.body.style.overflowX = "hidden";
+  } else {
+    missionText.classList.remove("from-right");
+    cameraImage.classList.remove("from-left");
+    document.body.style.overflowX = "auto";
+  }
+};
+const MissionObs = new IntersectionObserver(missionAppear, observerOption);
+MissionObs.observe(missionSection);
+
+// Pricing card appear
+const cardAppaer = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) {
+    card1.classList.remove("moveFromLeft");
+    card2.classList.remove("moveFromBottom");
+    card3.classList.remove("moveFromRight");
+    document.body.style.overflowX = "hidden";
+  } else {
+    card1.classList.add("moveFromLeft");
+    card2.classList.add("moveFromBottom");
+    card3.classList.add("moveFromRight");
+    document.body.style.overflowX = "hidden";
+  }
+};
+const pricingObs = new IntersectionObserver(cardAppaer, observerOption);
+pricingObs.observe(pricingSection);
+
 // window.onscroll = function () {
-
-//   if (
-//     document.body.scrollTop >= 800 ||
-//     document.documentElement.scrollTop >= 800
-//   ) {
-//     missionText.classList.add("from-right");
-//     cameraImage.classList.add("from-left");
-//     document.body.style.overflowX = "hidden";
-//   } else {
-//     missionText.classList.remove("from-right");
-//     cameraImage.classList.remove("from-left");
-//     document.body.style.overflowX = "auto";
-//   }
-
-//   if (
-//     document.body.scrollTop >= 3400 ||
-//     document.documentElement.scrollTop >= 3400
-//   ) {
-//     card1.classList.add("moveFromLeft");
-//     card2.classList.add("moveFromBottom");
-//     card3.classList.add("moveFromRight");
-//     document.body.style.overflowX = "hidden";
-//   } else {
-//     card1.classList.remove("moveFromLeft");
-//     card2.classList.remove("moveFromBottom");
-//     card3.classList.remove("moveFromRight");
-//     document.body.style.overflowX = "auto";
-//   }
 
 //   if (
 //     document.body.scrollTop >= 600 ||
