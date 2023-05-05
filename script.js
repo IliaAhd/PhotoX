d9c393b14c6b93558d44f8c6d4a38fe2b1c50fe0
IliaNoah <vaguaman86@gmail.com>
2023-05-05 16:57:15 +0430

    refactor sticky navbar using itersection observer

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+'use strict'
+
+// Items & elements
 const navButton = document.querySelector(".nav-button");
 const navMenu = document.querySelector(".nav-menu");
+const navItems = document.querySelectorAll(".nav-link");
+const banner = document.querySelector(".banner");
+
 const line1 = document.querySelector(".line1");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
@@ -11,8 +17,11 @@ const card1 = document.querySelector(".card-1");
 const card2 = document.querySelector(".card-2");
 const card3 = document.querySelector(".card-3");
 
+const galleryItems = document.querySelectorAll(".gallery-list-item");
+
 const date = document.querySelector(".date");
 
+// Hamburger menu
 navButton.addEventListener("click", function () {
   line1.classList.toggle("change");
   line2.classList.toggle("change");
@@ -20,8 +29,7 @@ navButton.addEventListener("click", function () {
   navMenu.classList.toggle("bg-dark");
 });
 
-const navItems = document.querySelectorAll(".nav-link");
-
+// Active nav item
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener("click", function () {
     for (let i = 0; i < navItems.length; i++) {
@@ -31,8 +39,7 @@ for (let i = 0; i < navItems.length; i++) {
   });
 }
 
-const galleryItems = document.querySelectorAll(".gallery-list-item");
-
+// Active gallery list item
 for (let i = 0; i < galleryItems.length; i++) {
   galleryItems[i].addEventListener("click", function () {
     for (let i = 0; i < galleryItems.length; i++) {
@@ -42,159 +49,167 @@ for (let i = 0; i < galleryItems.length; i++) {
   });
 }
 
-window.onscroll = function () {
-  if (
-    document.body.scrollTop >= 200 ||
-    document.documentElement.scrollTop >= 200
-  ) {
-    navMenu.classList.add("costume-navbar");
-  } else {
-    navMenu.classList.remove("costume-navbar");
-  }
+// Sticky Navbar
+const navChange = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
 
-  if (
-    document.body.scrollTop >= 800 ||
-    document.documentElement.scrollTop >= 800
-  ) {
-    missionText.classList.add("from-right");
-    cameraImage.classList.add("from-left");
-    document.body.style.overflowX = "hidden";
-  } else {
-    missionText.classList.remove("from-right");
-    cameraImage.classList.remove("from-left");
-    document.body.style.overflowX = "auto";
-  }
-
-  if (
-    document.body.scrollTop >= 3400 ||
-    document.documentElement.scrollTop >= 3400
-  ) {
-    card1.classList.add("moveFromLeft");
-    card2.classList.add("moveFromBottom");
-    card3.classList.add("moveFromRight");
-    document.body.style.overflowX = "hidden";
-  } else {
-    card1.classList.remove("moveFromLeft");
-    card2.classList.remove("moveFromBottom");
-    card3.classList.remove("moveFromRight");
-    document.body.style.overflowX = "auto";
-  }
-
-  if (
-    document.body.scrollTop >= 600 ||
-    document.documentElement.scrollTop >= 600
-  ) {
-    document.querySelector(".nav-zero").classList.remove("nav-active");
-  } else if (
-    document.body.scrollTop <= 600 ||
-    document.documentElement.scrollTop <= 600
-  ) {
-    document.querySelector(".nav-zero").classList.add("nav-active");
-  }
-
-  if (
-    (600 <= document.body.scrollTop && document.body.scrollTop <= 1600) ||
-    (600 <= document.documentElement.scrollTop &&
-      document.documentElement.scrollTop <= 1600)
-  ) {
-    document.querySelector(".nav-one").classList.add("nav-active");
-  } else if (
-    (600 >= document.body.scrollTop && document.body.scrollTop >= 1600) ||
-    600 >= document.documentElement.scrollTop ||
-    document.documentElement.scrollTop >= 1600
-  ) {
-    document.querySelector(".nav-one").classList.remove("nav-active");
-  }
-
-  if (
-    (1600 <= document.body.scrollTop && document.body.scrollTop <= 2700) ||
-    (1600 <= document.documentElement.scrollTop &&
-      document.documentElement.scrollTop <= 2700)
-  ) {
-    document.querySelector(".nav-three").classList.add("nav-active");
-  } else if (
-    (1600 >= document.body.scrollTop && document.body.scrollTop >= 2700) ||
-    1600 >= document.documentElement.scrollTop ||
-    document.documentElement.scrollTop >= 2700
-  ) {
-    document.querySelector(".nav-three").classList.remove("nav-active");
-  }
-
-  if (
-    (2700 <= document.body.scrollTop && document.body.scrollTop <= 3500) ||
-    (2700 <= document.documentElement.scrollTop &&
-      document.documentElement.scrollTop <= 3500)
-  ) {
-    document.querySelector(".nav-four").classList.add("nav-active");
-  } else if (
-    (2700 >= document.body.scrollTop && document.body.scrollTop >= 3500) ||
-    2700 >= document.documentElement.scrollTop ||
-    document.documentElement.scrollTop >= 3500
-  ) {
-    document.querySelector(".nav-four").classList.remove("nav-active");
-  }
-
-  if (
-    (3500 <= document.body.scrollTop && document.body.scrollTop <= 4300) ||
-    (3500 <= document.documentElement.scrollTop &&
-      document.documentElement.scrollTop <= 4300)
-  ) {
-    document.querySelector(".nav-five").classList.add("nav-active");
-  } else if (
-    (3500 >= document.body.scrollTop && document.body.scrollTop >= 4300) ||
-    3500 >= document.documentElement.scrollTop ||
-    document.documentElement.scrollTop >= 4300
-  ) {
-    document.querySelector(".nav-five").classList.remove("nav-active");
-  }
-
-  if (
-    (4300 <= document.body.scrollTop && document.body.scrollTop <= 5700) ||
-    (4300 <= document.documentElement.scrollTop &&
-      document.documentElement.scrollTop <= 5700)
-  ) {
-    document.querySelector(".nav-six").classList.add("nav-active");
-  } else if (
-    (4300 >= document.body.scrollTop && document.body.scrollTop >= 5700) ||
-    4300 >= document.documentElement.scrollTop ||
-    document.documentElement.scrollTop >= 5700
-  ) {
-    document.querySelector(".nav-six").classList.remove("nav-active");
-  }
-
-  if (
-    document.body.scrollTop >= 5700 ||
-    document.documentElement.scrollTop >= 5700
-  ) {
-    document.querySelector(".nav-seven").classList.add("nav-active");
-  } else if (
-    document.body.scrollTop <= 5700 ||
-    document.documentElement.scrollTop <= 5700
-  ) {
-    document.querySelector(".nav-seven").classList.remove("nav-active");
-  }
-
-  if (
-    document.body.scrollTop >= 5200 ||
-    document.documentElement.scrollTop >= 5200
-  ) {
-    for (let i = 0; i < galleryItems.length; i++) {
-      for (let i = 0; i < galleryItems.length; i++) {
-        galleryItems[i].classList.remove("active-item");
-      }
-    }
-    document.querySelector(".gallery-list-item").classList.add("active-item");
-
-    const filter = document.querySelectorAll(".filter");
-
-    for (let i = 0; i < filter.length; i++) {
-      filter[i].style.display = "block";
-      filter[i].style.opacity = "1";
-    }
-  }
-  console.log(scrollY);
+  if (!entry.isIntersecting) navMenu.classList.add("costume-navbar");
+  else navMenu.classList.remove("costume-navbar");
 };
 
+const navObserver = new IntersectionObserver(navChange, {
+  root: null,
+  treshold: 0.1,
+});
+navObserver.observe(banner);
+
+// window.onscroll = function () {
+
+//   if (
+//     document.body.scrollTop >= 800 ||
+//     document.documentElement.scrollTop >= 800
+//   ) {
+//     missionText.classList.add("from-right");
+//     cameraImage.classList.add("from-left");
+//     document.body.style.overflowX = "hidden";
+//   } else {
+//     missionText.classList.remove("from-right");
+//     cameraImage.classList.remove("from-left");
+//     document.body.style.overflowX = "auto";
+//   }
+
+//   if (
+//     document.body.scrollTop >= 3400 ||
+//     document.documentElement.scrollTop >= 3400
+//   ) {
+//     card1.classList.add("moveFromLeft");
+//     card2.classList.add("moveFromBottom");
+//     card3.classList.add("moveFromRight");
+//     document.body.style.overflowX = "hidden";
+//   } else {
+//     card1.classList.remove("moveFromLeft");
+//     card2.classList.remove("moveFromBottom");
+//     card3.classList.remove("moveFromRight");
+//     document.body.style.overflowX = "auto";
+//   }
+
+//   if (
+//     document.body.scrollTop >= 600 ||
+//     document.documentElement.scrollTop >= 600
+//   ) {
+//     document.querySelector(".nav-zero").classList.remove("nav-active");
+//   } else if (
+//     document.body.scrollTop <= 600 ||
+//     document.documentElement.scrollTop <= 600
+//   ) {
+//     document.querySelector(".nav-zero").classList.add("nav-active");
+//   }
+
+//   if (
+//     (600 <= document.body.scrollTop && document.body.scrollTop <= 1600) ||
+//     (600 <= document.documentElement.scrollTop &&
+//       document.documentElement.scrollTop <= 1600)
+//   ) {
+//     document.querySelector(".nav-one").classList.add("nav-active");
+//   } else if (
+//     (600 >= document.body.scrollTop && document.body.scrollTop >= 1600) ||
+//     600 >= document.documentElement.scrollTop ||
+//     document.documentElement.scrollTop >= 1600
+//   ) {
+//     document.querySelector(".nav-one").classList.remove("nav-active");
+//   }
+
+//   if (
+//     (1600 <= document.body.scrollTop && document.body.scrollTop <= 2700) ||
+//     (1600 <= document.documentElement.scrollTop &&
+//       document.documentElement.scrollTop <= 2700)
+//   ) {
+//     document.querySelector(".nav-three").classList.add("nav-active");
+//   } else if (
+//     (1600 >= document.body.scrollTop && document.body.scrollTop >= 2700) ||
+//     1600 >= document.documentElement.scrollTop ||
+//     document.documentElement.scrollTop >= 2700
+//   ) {
+//     document.querySelector(".nav-three").classList.remove("nav-active");
+//   }
+
+//   if (
+//     (2700 <= document.body.scrollTop && document.body.scrollTop <= 3500) ||
+//     (2700 <= document.documentElement.scrollTop &&
+//       document.documentElement.scrollTop <= 3500)
+//   ) {
+//     document.querySelector(".nav-four").classList.add("nav-active");
+//   } else if (
+//     (2700 >= document.body.scrollTop && document.body.scrollTop >= 3500) ||
+//     2700 >= document.documentElement.scrollTop ||
+//     document.documentElement.scrollTop >= 3500
+//   ) {
+//     document.querySelector(".nav-four").classList.remove("nav-active");
+//   }
+
+//   if (
+//     (3500 <= document.body.scrollTop && document.body.scrollTop <= 4300) ||
+//     (3500 <= document.documentElement.scrollTop &&
+//       document.documentElement.scrollTop <= 4300)
+//   ) {
+//     document.querySelector(".nav-five").classList.add("nav-active");
+//   } else if (
+//     (3500 >= document.body.scrollTop && document.body.scrollTop >= 4300) ||
+//     3500 >= document.documentElement.scrollTop ||
+//     document.documentElement.scrollTop >= 4300
+//   ) {
+//     document.querySelector(".nav-five").classList.remove("nav-active");
+//   }
+
+//   if (
+//     (4300 <= document.body.scrollTop && document.body.scrollTop <= 5700) ||
+//     (4300 <= document.documentElement.scrollTop &&
+//       document.documentElement.scrollTop <= 5700)
+//   ) {
+//     document.querySelector(".nav-six").classList.add("nav-active");
+//   } else if (
+//     (4300 >= document.body.scrollTop && document.body.scrollTop >= 5700) ||
+//     4300 >= document.documentElement.scrollTop ||
+//     document.documentElement.scrollTop >= 5700
+//   ) {
+//     document.querySelector(".nav-six").classList.remove("nav-active");
+//   }
+
+//   if (
+//     document.body.scrollTop >= 5700 ||
+//     document.documentElement.scrollTop >= 5700
+//   ) {
+//     document.querySelector(".nav-seven").classList.add("nav-active");
+//   } else if (
+//     document.body.scrollTop <= 5700 ||
+//     document.documentElement.scrollTop <= 5700
+//   ) {
+//     document.querySelector(".nav-seven").classList.remove("nav-active");
+//   }
+
+//   if (
+//     document.body.scrollTop >= 5200 ||
+//     document.documentElement.scrollTop >= 5200
+//   ) {
+//     for (let i = 0; i < galleryItems.length; i++) {
+//       for (let i = 0; i < galleryItems.length; i++) {
+//         galleryItems[i].classList.remove("active-item");
+//       }
+//     }
+//     document.querySelector(".gallery-list-item").classList.add("active-item");
+
+//     const filter = document.querySelectorAll(".filter");
+
+//     for (let i = 0; i < filter.length; i++) {
+//       filter[i].style.display = "block";
+//       filter[i].style.opacity = "1";
+//     }
+//   }
+//   console.log(scrollY);
+// };
+
+// Gallery filter
 document.addEventListener("DOMContentLoaded", function () {
   var galleryListItems = document.querySelectorAll(".gallery-list-item");
   galleryListItems.forEach(function (item) {
@@ -229,4 +244,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Date
 date.textContent = new Date().getFullYear();
